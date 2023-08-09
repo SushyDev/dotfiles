1b0b4b16e248db94b7c9f23f1a843fcefa766a95
Sushy <43114803+SushyDev@users.noreply.github.com>
2023-08-09 21:48:42 +0200

    Update install.sh

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,6 +1,7 @@
 # Install modules
 git -C $DOTFILES submodule update --init --recursive
 
+# Create symlink
 rm -rf "$DOTFILES/zsh/oh-my-zsh/custom"
 ln -s "$DOTFILES/zsh/custom" "$DOTFILES/zsh/oh-my-zsh/custom"
 
