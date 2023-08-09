b35d389c2966928de52f986f47dc519b297cd019
SushyDev <xsushy@hotmail.com>
2023-08-09 21:28:26 +0200

    install script

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,3 +1,6 @@
+# Install modules
+git submodule update --init --recursive
+
 # Symlink custom folder
 rm -rf "$DOTFILES/zsh/oh-my-zsh/custom"
 ln -s "$DOTFILES/zsh/custom" "$DOTFILES/zsh/oh-my-zsh/custom"
